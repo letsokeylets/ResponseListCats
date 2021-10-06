0a78ea4883d9e9a1a7168c2dd9cf6bc52ecd0d50
Mozdir Taras <letsokey@mail.ru>
2021-10-06 18:54:49 +0400

    Create json response parser

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,56 @@
 package org.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
- * Hello world!
- *
+ * Основной мэин класс
  */
-public class App 
-{
-    public static void main( String[] args )
+
+public class App {
+
+    public static final ObjectMapper mapper = new ObjectMapper();
+
     {
-        System.out.println( "Hello World!" );
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = createHttpClient();
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        //Преобразование json в список java объектов
+        List<Response> posts = mapper.readValue(
+                response.getEntity().getContent(), new TypeReference<List<Response>>() {
+                });
+        posts.stream()
+                //Фильтрация списка (upvotes > 0 и type - "Кошка")
+                .filter(value -> value.getUpvotes() > 0 && "cat".equals(value.getType()))
+                //вывод на экран
+                .forEach(System.out::println);
+    }
+
+    /**
+     * Метод создаёт httpClient
+     */
+    private static CloseableHttpClient createHttpClient() {
+        return HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
     }
 }
---
+++ src/main/java/org/example/Response.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+/**
+ * Класс для преобразование json в список
+ */
+
+public class Response {
+
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upvotes;
+
+    public Response(@JsonProperty("id") String id,
+                    @JsonProperty("text") String text,
+                    @JsonProperty("type") String type,
+                    @JsonProperty("user") String user,
+                    @JsonProperty("upvotes") int upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes=" + upvotes +
+                '}';
+    }
+}
